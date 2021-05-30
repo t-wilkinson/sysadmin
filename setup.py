8b43db625ccf11263c74685b10c81a2dbfa65104
t-wilkinson <winston.trey.wilkinson@gmail.com>
2021-05-30 06:21:45 -0400

    [init]

---
+++ setup.py
@@ -0,0 +1,24 @@
+mport setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sysadmin",
+    version="0.0.1",
+    author="t-wilkinson",
+    author_email="winston.trey.wilkinson@gmail.com",
+    description="Packages to help with sys admin",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/t-wilkinson/sysadmin",
+    project_urls={
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    package_dir={"": "sysadmin"},
+    packages=setuptools.find_packages(where="sysadmin"),
+    python_requires=">=3.6",
+)
